ea7942df87ee87292b5b1d8f12c0ff86194bf23c
Aleksandr <maivan007@yandex.ru>
2022-06-23 21:51:16 +0400

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿int number =  Convert.ToInt32(Console.ReadLine()); //32 бит, 64 бит 
+Console.WriteLine(number*number);
+//Console.WriteLine(Math.Pow(number, 2));
+Console.WriteLine(number + " квадратное равно " + number*number);
+Console.WriteLine($"{number} квадрате равно {number*number}");
