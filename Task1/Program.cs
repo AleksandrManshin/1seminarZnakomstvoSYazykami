a7e4d35689f29be9d840513b8cbc6b05428d15e4
Aleksandr <maivan007@yandex.ru>
2022-06-24 11:39:02 +0400

    Добавил правки в первую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,10 @@
-﻿int number =  Convert.ToInt32(Console.ReadLine()); //32 бит, 64 бит 
-Console.WriteLine(number*number);
+﻿// <тип_данных> <имя переменной> = <значение>;
+// <тип_данных> <имя переменной>;
+//int number = 5; //integer
+Console.WriteLine("Введите число которое нужно возвести в квадрат ");
+int number = Convert.ToInt32(Console.ReadLine());//32 бит, 64 бит 
+//Console.WriteLine(number*number);
 //Console.WriteLine(Math.Pow(number, 2));
-Console.WriteLine(number + " квадратное равно " + number*number);
-Console.WriteLine($"{number} квадрате равно {number*number}");
+//Console.WriteLine(number + " в квадрате равно " + Math.Pow(number, 2));
+Console.WriteLine($"{number} в квадрате равно  {number*number}");
+//Console.WriteLine($"{number} 
