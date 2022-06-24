1aa51e55e7b95e830edc8888a92c520498621c86
Aleksandr <maivan007@yandex.ru>
2022-06-24 17:02:05 +0400

    Добавил 4 задачу

---
+++ Task4/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+void OurPower(int currentNumber)
+{
+    Console.WriteLine(currentNumber * currentNumber);
+}
+OurPower(number);
+OurPower(7);
+OurPower(number + number);
