7cd3c8171b2f36d58ff1b7b76253e2dbfcb26a77
Aleksandr <maivan007@yandex.ru>
2022-06-24 11:59:58 +0400

    Добавил вторую задачу

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Проверка. Является ли первое число квадратом второго.");
+Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == Math.Pow(numberB, 2))
+{
+    Console.WriteLine("Да, является. ");
+}
+else
+{
+    Console.WriteLine("Нет, не является. ");
+}
