398a7d41db48469e1b073c9326b4e05497b3ae9d
Aleksandr <maivan007@yandex.ru>
2022-06-26 15:37:11 +0400

    Добавил задачу 5

---
+++ Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = -4; //-4 -3 -2 -1 0 1 2 3 4 
+number = Math.Abs(number);
+int count = -number;
+while (count <= number)
+{
+    Console.Write(count + "  ");
+    count ++;
+}
+Console.WriteLine();
